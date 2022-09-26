11554b538866f5932ffb9c3e50ebbf53b8d76df9
loganwyas <loganw9999@gmail.com>
2022-09-26 15:57:17 -0500

    WIP (Add new images for buttons)

--- main.py
+++ main.py
@@ -11,19 +11,27 @@ title = Label(main, text="Mouser", font=("Arial", 25))
 title.grid(row=0, column=0, columnspan=4, sticky=N)
 
 mouse = PhotoImage(file="./images/mouse_small.png")
+graph = PhotoImage(file="./images/graph_small.png")
+magnify = PhotoImage(file="./images/magnifier_small.png")
+user = PhotoImage(file="./images/user_small.png")
 
-animal_setup = Button(main, text="Animal Setup", image=mouse, compound=TOP)
-animal_setup.grid(row=1, column=0, columnspan=2)
+buttonDim = 25
+
+animal_setup = Button(main, text="Animal Setup", image=mouse,
+                      compound=TOP, width=buttonDim)
+animal_setup.place(relx=0.33, rely=0.33, anchor=CENTER)
 
 data_collection = Button(main, text="Data Collection",
-                         image=mouse, compound=TOP)
-data_collection.grid(row=1, column=2, columnspan=2)
+                         image=graph, compound=TOP, width=buttonDim)
+data_collection.place(relx=0.67, rely=0.33, anchor=CENTER)
 
-analysis = Button(main, text="Analysis", image=mouse, compound=TOP)
-analysis.grid(row=2, column=0, columnspan=2)
+analysis = Button(main, text="Analysis", image=magnify,
+                  compound=TOP, width=buttonDim)
+analysis.place(relx=0.33, rely=0.67, anchor=CENTER)
 
-accounts = Button(main, text="Accounts", image=mouse, compound=TOP)
-accounts.grid(row=2, column=2, columnspan=2)
+accounts = Button(main, text="Accounts", image=user,
+                  compound=TOP, width=buttonDim)
+accounts.place(relx=0.67, rely=0.67, anchor=CENTER)
 
 main.grid(row=0, column=0, sticky="NESW")
 main.grid_rowconfigure(0, weight=1)
