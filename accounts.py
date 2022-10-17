5434439d16f379bc840a1c33655b2a5f476da4d1
loganwyas <loganw9999@gmail.com>
2022-10-17 15:53:36 -0500

    WIP (Add user configuration page)

--- accounts.py
+++ accounts.py
@@ -1,54 +1,85 @@
 from tkinter import *
 from tkinter.ttk import *
 from tk_models import *
-import os
+import copy
 
 users = {
     'example.student@slu.edu': {
         "first": "Example",
-        "last": "Student"
+        "last": "Student",
+        "role": "generic"
     },
     'another.person@slu.edu': {
         "first": "Another",
-        "last": "Person"
+        "last": "Person",
+        "role": "admin"
     },
     'test@slu.edu': {
         "first": "Test",
-        "last": "Person"
+        "last": "Person",
+        "role": "generic"
     }
 }
+
+roles = ["generic", "admin"]
 
 
 class ConfigureUsersFrame(MouserPage):
     def __init__(self, parent: Tk, previous_page: Frame):
         super().__init__(parent, "User Configuration", True, previous_page)
 
-        Label(self, text="Select a User:").place(
-            relx=0.15, rely=0.30, anchor=CENTER)
+        Label(self, text="Email:").place(
+            relx=0.12, rely=0.30)
 
         self.user_select = Combobox(self, width=27)
         self.user_select['values'] = tuple(users.keys())
         self.user_select['state'] = 'readonly'
-        self.user_select.place(relx=0.40, rely=0.30, anchor=CENTER)
+        self.user_select.place(relx=0.25, rely=0.30)
         self.user_select.bind('<<ComboboxSelected>>', self.display_information)
 
-        self.first_name_label = Label(self)
-        self.last_name_label = Label(self)
+        self.name_header = Label(self)
+        self.name_label = Label(self)
+        self.access_header = Label(self)
+        self.access_select = Combobox(self)
 
     def display_information(self, event):
+
         selected = self.user_select.get()
+        self.selected = selected
+        self.changed_data = copy.deepcopy(users)
 
-        self.first_name_label.destroy()
-        first_name_text = "First Name:      " + users[selected]["first"]
-        self.first_name_label = Label(self, text=first_name_text)
-        self.first_name_label.place(
-            relx=0.25, rely=0.50)
+        self.access_header.destroy()
+        self.access_header = Label(self, text="Access:")
+        self.access_header.place(relx=0.12, rely=0.50)
 
-        self.last_name_label.destroy()
-        last_name_text = "Last Name:      " + users[selected]["last"]
-        self.last_name_label = Label(self, text=last_name_text)
-        self.last_name_label.place(
-            relx=0.25, rely=0.55)
+        self.access_select.destroy()
+        self.access_select = Combobox(self, width=27)
+        self.access_select['values'] = tuple(roles)
+        self.access_select['state'] = 'readonly'
+        self.access_select.set(users[selected]["role"])
+        self.access_select.place(relx=0.25, rely=0.50)
+        self.access_select.bind('<<ComboboxSelected>>', self.detect_changes)
+
+        self.name_header.destroy()
+        self.name_header = Label(self, text="Name:")
+        self.name_header.place(relx=0.12, rely=0.40)
+
+        self.name_label.destroy()
+        name_text = users[selected]["first"] + " " + users[selected]["last"]
+        self.name_label = Label(self, text=name_text)
+        self.name_label.place(relx=0.25, rely=0.40)
+
+    def detect_changes(self, event):
+        self.changed_data[self.selected]["role"] = self.access_select.get()
+        user = users[self.selected]
+        changed = False
+        for key in user.keys():
+            if (user[key] != self.changed_data[self.selected][key]):
+                changed = True
+        if (changed):
+            self.user_select['state'] = "disabled"
+        else:
+            self.user_select['state'] = "readonly"
 
 
 class ChangePasswordFrame(MouserPage):
