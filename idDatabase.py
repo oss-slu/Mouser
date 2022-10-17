7ea4dd1cb203ebaa962a45e3363c6f9149e25708
Tyler Bush <tyler.bush.1@slu.edu>
2022-10-17 15:27:56 -0500

    python style

--- idDatabase.py
+++ idDatabase.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-class idDatabase:
+class Id_Database:
     def __init__(self):
         self._conn = sqlite3.connect(':memory:') #can input file name instead of memory, need to add that functionality later.
         self._c = self._conn.cursor()
@@ -13,15 +13,15 @@ class idDatabase:
         except:
             pass
  
-    def addAnimal(self, rf, an):          #problem - can add animals with same rfid and/or animalid
+    def add_animal(self, rf, an):          #problem - can add animals with same rfid and/or animalid
         self._c.execute("INSERT INTO conversion VALUES (?, ?)", (rf, an))
         self._conn.commit()
 
-    def getAnimals(self):
+    def get_all_animals(self):
         self._c.execute("SELECT * FROM conversion")
         return self._c.fetchall()
 
-    def getAnimalId(self, rf):
+    def get_animal_id(self, rf):
         self._c.execute("SELECT animalid FROM conversion WHERE rfid=?", (rf,))
         return self._c.fetchone()[0]
 
@@ -30,7 +30,7 @@ class idDatabase:
 
 
 if __name__ == "__main__":
-    db = idDatabase()
-    db.addAnimal(1234, 1111)
-    print(db.getAnimalId(1234))
-    print(db.getAnimals())
+    db = Id_Database()
+    db.add_animal(1234, 1111)
+    print(db.get_animal_id(1234))
+    print(db.get_all_animals())
