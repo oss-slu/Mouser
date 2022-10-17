1210b655e82ddc0b3caf2d19d2f6e978578a7105
Tyler Bush <tyler.bush.1@slu.edu>
2022-10-17 15:24:06 -0500

    add get animal id function

--- idDatabase.py
+++ idDatabase.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class idDatabase:
     def __init__(self):
-        self._conn = sqlite3.connect('conversion.db')
+        self._conn = sqlite3.connect(':memory:') #can input file name instead of memory, need to add that functionality later.
         self._c = self._conn.cursor()
         try:
             self._c.execute('''CREATE TABLE conversion (
@@ -21,6 +21,10 @@ class idDatabase:
         self._c.execute("SELECT * FROM conversion")
         return self._c.fetchall()
 
+    def getAnimalId(self, rf):
+        self._c.execute("SELECT animalid FROM conversion WHERE rfid=?", (rf,))
+        return self._c.fetchone()[0]
+
     def close(self):
         self._conn.close()
 
@@ -28,4 +32,5 @@ class idDatabase:
 if __name__ == "__main__":
     db = idDatabase()
     db.addAnimal(1234, 1111)
+    print(db.getAnimalId(1234))
     print(db.getAnimals())
