d8ed787e737254c4e7b197c258d15e5d75c078f7
Cori Diaz <corinicolediaz@gmail.com>
2022-11-09 21:48:10 -0600

    fixed checkbutton, all items on page, wip:expansion of add investigators and measurement items

--- new_exper_ui.py
+++ new_exper_ui.py
@@ -3,6 +3,7 @@ from tkinter.ttk import *
 from tk_models import *
 from group_config_ui import GroupConfigUI
 
+investigators = ['investigator a', 'investigator b', 'investigator c']
 
 class NewExperimentUI(MouserPage):
     def __init__(self, parent:Tk, prev_page: Frame = None):
@@ -10,28 +11,66 @@ class NewExperimentUI(MouserPage):
 
         self.set_next_button(GroupConfigUI(parent, self))
 
-        main_frame = Frame(self)
-        main_frame.grid(row=9, column=4, sticky='NESW')
-        main_frame.place(relx=0.12, rely=0.20)
+        self.main_frame = Frame(self)
+        self.main_frame.grid(row=10, column=3, sticky='NESW')
+        self.main_frame.place(relx=0.12, rely=0.20)
+
+        self.invest_frame = Frame(self.main_frame)
+        self.item_frame = Frame(self.main_frame)
+        self.rfid_frame = Frame(self.main_frame)
+
+        self.invest_frame.grid(row=2, column=1, sticky='NESW')
+        self.item_frame.grid(row=5, column=1, sticky='NESW')
+        self.rfid_frame.grid(row=6, column=1, sticky='NESW')
 
         pad_x = 10
         pad_y = 10
 
-        Label(main_frame, text='Experiment Name').grid(row=1, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text='Investigators').grid(row=2, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text='Species').grid(row=4, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text='Measurement Items').grid(row=5, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text="RFID").grid(row=7, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text="Number of Animals").grid(row=8, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text="Number of Groups").grid(row=9, column=0, sticky=W, padx=pad_x, pady=pad_y)
-        Label(main_frame, text="Max Animals per Cage").grid(row=10, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        self.items = []
+        self.added_invest = []
 
-        self.exper_name = Combobox(main_frame, width=30)
+        Label(self.main_frame, text='Experiment Name').grid(row=0, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text='Investigators').grid(row=1, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text='Species').grid(row=3, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text='Measurement Items').grid(row=4, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text="RFID").grid(row=6, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text="Number of Animals").grid(row=7, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text="Number of Groups").grid(row=8, column=0, sticky=W, padx=pad_x, pady=pad_y)
+        Label(self.main_frame, text="Max Animals per Cage").grid(row=9, column=0, sticky=W, padx=pad_x, pady=pad_y)
 
+        self.exper_name = Entry(self.main_frame, width=40)
+        self.investigators = Combobox(self.main_frame, values=investigators, width=37)
+        self.species = Entry(self.main_frame, width=40)
+        self.measure_items = Entry(self.main_frame, width=40)
+        self.animal_num = Entry(self.main_frame, width=10)
+        self.group_num = Entry(self.main_frame, width=10)
+        self.num_per_cage = Entry(self.main_frame, width=10)
 
+        self.exper_name.grid(row=0, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.investigators.grid(row=1, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.species.grid(row=3, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.measure_items.grid(row=4, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.animal_num.grid(row=7, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.group_num.grid(row=8, column=1, sticky=W, padx=pad_x, pady=pad_y)
+        self.num_per_cage.grid(row=9, column=1, sticky=W, padx=pad_x, pady=pad_y)
 
-        for i in range(0,9):
-            if i < 4:
+        self.rfid = BooleanVar()
+        Radiobutton(self.rfid_frame, text='Yes', variable=self.rfid, val=True).grid(row=0, column=0, 
+                    padx=pad_x, pady=pad_y)
+        Radiobutton(self.rfid_frame, text='No', variable=self.rfid, val=False).grid(row=0, column=1, 
+                    padx=pad_x, pady=pad_y)
+
+        add_invest_button = Button(self.main_frame, text='+', width=3, 
+                            command= lambda: self.update_invest_frame('add', None))
+        add_invest_button.grid(row=1, column=2, padx=pad_x, pady=pad_y)
+
+        add_item_button = Button(self.main_frame, text='+', width=3, 
+                            command= lambda: [self.update_items_frame('add', None), self.measure_items.delete(0, END)])
+        add_item_button.grid(row=4, column=2, padx=pad_x, pady=pad_y)
+        
+        
+        for i in range(0,10):
+            if i < 3:
                 self.grid_columnconfigure(i, weight=1)
             self.grid_rowconfigure(i, weight=1)
 
@@ -40,4 +79,55 @@ class NewExperimentUI(MouserPage):
         if self.next_button:
             self.next_button.destroy()
         self.next_button = ChangePageButton(self, next_page, False)
+        self.next_button.configure(command= lambda: [self.save_input(), self.next_button.navigate()])
         self.next_button.place(relx=0.85, rely=0.15)
+
+
+    def update_invest_frame(self, action, person):
+        # fix frame reset
+        # fix only display 1 invest
+        if person == None:
+            person = self.investigators.get()
+        else:
+            person = person
+        if action == 'add' and person not in self.added_invest:
+            self.added_invest.append(person)
+        elif action == 'remove' and person in self.added_invest:
+            self.added_invest.remove(person)
+        for investigator in self.added_invest:
+            Label(self.invest_frame, text=investigator).grid(row=self.added_invest.index(investigator), 
+                    column=1, sticky=W, padx=10)
+            Button(self.invest_frame, text='-', width=3, 
+                    command= lambda: self.update_invest_frame('remove', investigator)).grid(
+                    row=self.added_invest.index(investigator), column=2, padx=10)
+
+
+    def update_items_frame(self, action, item):
+        # fix default text dimensions
+        # fix item removal -> make labels/buttons identifiable
+        # add text boxes for each dimension
+        for widget in self.item_frame.winfo_children():
+            widget.destroy()
+        if item == None:
+            item = self.measure_items.get()
+        else:
+            item = item
+        if action == 'add' and item not in self.items and item != '':
+            self.items.append(item)
+        elif action == 'remove' and item in self.items:
+            self.items.remove(item)
+            print(self.items)
+        for meas_item in self.items:
+            Label(self.item_frame, text=meas_item).grid(
+                row=self.items.index(meas_item), column=0, sticky=W, padx=10, pady=10)
+            Entry(self.item_frame, textvariable='# of dimensions').grid(row=self.items.index(meas_item), 
+                column=1, sticky=W, padx=10, pady=10)
+            Button(self.item_frame, text='-', width=3, 
+                command= lambda: self.update_items_frame('remove', meas_item)).grid(row=self.items.index(meas_item), 
+                column=2, sticky=W, padx=10, pady=10)
+
+
+    def save_input(self):
+        print(self.exper_name.get())
+        print(self.measure_items)
+        print('saved')
