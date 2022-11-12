87cd374130c3cf360c9edd65b821902c78a2204f
Tyler Bush <tyler.bush.1@slu.edu>
2022-11-12 15:45:02 -0600

    finalize database tables, create experiment func

--- Database.py
+++ Database.py
@@ -6,6 +6,7 @@ class Database:
         self._c = self._conn.cursor()
         try:
             self._c.execute('''CREATE TABLE experiment (
+                                experiment_id INTEGER PRIMARY KEY,
                                 name TEXT,
                                 species TEXT,
                                 uses_rfid INTEGER,
@@ -14,40 +15,59 @@ class Database:
                                 cage_max INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE animals (
-                                animal_id INTEGER,
+                                animal_id INTEGER PRIMARY KEY,
                                 group_id INTEGER,
                                 cage_id INTEGER,
                                 remarks TEXT
                                 );''')
             self._c.execute('''CREATE TABLE groups (
-                                group_id INTEGER,
+                                group_id INTEGER PRIMARY KEY,
                                 name TEXT,
                                 num_animals INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE cages (
-                                cage_id INTEGER,
+                                cage_id INTEGER PRIMARY KEY,
                                 group_id INTEGER,
                                 num_animals INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE measurement_items (
-                                measurement_id INTEGER,
+                                measurement_id INTEGER PRIMARY KEY,
                                 item TEXT,
                                 auto INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE conversion (
-                                rfid TEXT NOT NULL UNIQUE,
-                                animal_id INTEGER NOT NULL PRIMARY KEY
+                                animal_id INTEGER PRIMARY KEY,
+                                rfid TEXT UNIQUE
                                 );''')
             self._conn.commit()
-        except:
-            pass
+        except sqlite3.OperationalError:
+            print('Tables already exist')
+
  
     def create_experiment(self, name, species, uses_rfid, num_animals, group_names, cage_max):
+        #insert into experiment table
+        num_groups = len(group_names)
+        self._c.execute(''' INSERT INTO experiment (name, species, uses_rfid, num_animals, num_groups, cage_max) 
+                            VALUES (?, ?, ?, ?, ?, ?)''',
+                            (name, species, uses_rfid, num_animals, num_groups, cage_max))
+        self._conn.commit()
+
+        #insert into groups table
+        animals_per_group = num_animals / num_groups
+        for group_name in group_names:
+            self._c.execute(''' INSERT INTO groups (name, num_animals) 
+                                VALUES (?, ?)''',
+                                (group_name, animals_per_group))
+            self._conn.commit()
+            
+        #insert into cage table
+
+        #insert into measurement items table
+        
         
 
     def add_animal(self, rf):
-        an = self.get_next_animal_id()
-        self._c.execute("INSERT INTO conversion VALUES (?, ?)", (rf, an))
+        self._c.execute("INSERT INTO conversion (rfid) VALUES (?)", (rf, ))
         self._conn.commit()
 
     def get_all_animals(self):
@@ -62,19 +82,10 @@ class Database:
         self._conn.close()
 
 
-    def get_next_animal_id(self):
-        self._c.execute("SELECT animal_id FROM conversion")
-        ids = self._c.fetchall()
-        num = 0
-        for i in ids:
-            if (i[0] > num):
-                num = i[0]
-        return num+1
-
-
-
 if __name__ == "__main__":
     db = Database()
+    db.create_experiment('CancerDrug', 'Hamster', 1, 90, ('Control', 'Drug A', 'Drug B'), 5)
+
     db.add_animal(1234)
     db.add_animal(4562)
     db.add_animal(4682)
