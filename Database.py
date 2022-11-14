ff0d2548f85ae08ca30e431b7798cdd82aa6b11b
Tyler Bush <tyler.bush.1@slu.edu>
2022-11-14 14:55:35 -0600

    refactored experiment setup and add animal funcs

--- Database.py
+++ Database.py
@@ -15,27 +15,32 @@ class Database:
                                 cage_max INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE animals (
+                                experiment_id INTEGER
                                 animal_id INTEGER PRIMARY KEY,
                                 group_id INTEGER,
                                 cage_id INTEGER,
                                 remarks TEXT
                                 );''')
             self._c.execute('''CREATE TABLE groups (
+                                experiment_id INTEGER
                                 group_id INTEGER PRIMARY KEY,
                                 name TEXT,
                                 num_animals INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE cages (
+                                experiment_id INTEGER
                                 cage_id INTEGER PRIMARY KEY,
                                 group_id INTEGER,
                                 num_animals INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE measurement_items (
+                                experiment_id INTEGER
                                 measurement_id INTEGER PRIMARY KEY,
                                 item TEXT,
                                 auto INTEGER
                                 );''')
             self._c.execute('''CREATE TABLE conversion (
+                                experiment_id INTEGER
                                 animal_id INTEGER PRIMARY KEY,
                                 rfid TEXT UNIQUE
                                 );''')
@@ -44,30 +49,33 @@ class Database:
             print('Tables already exist')
 
  
-    def create_experiment(self, name, species, uses_rfid, num_animals, group_names, cage_max):
-        #insert into experiment table
-        num_groups = len(group_names)
+    def setup_experiment(self, name, species, uses_rfid, num_animals, num_groups, cage_max):
         self._c.execute(''' INSERT INTO experiment (name, species, uses_rfid, num_animals, num_groups, cage_max) 
                             VALUES (?, ?, ?, ?, ?, ?)''',
                             (name, species, uses_rfid, num_animals, num_groups, cage_max))
         self._conn.commit()
 
-        #insert into groups table
-        animals_per_group = num_animals / num_groups
-        for group_name in group_names:
+    def setup_groups(self, group_names, animals_per_group):
+        for group in group_names:
             self._c.execute(''' INSERT INTO groups (name, num_animals) 
                                 VALUES (?, ?)''',
-                                (group_name, animals_per_group))
+                                (group, animals_per_group))
             self._conn.commit()
-            
-        #insert into cage table
+    
+    def setup_cages(self):
+        pass
 
-        #insert into measurement items table
-        
+    def setup_measurement_items(self):
+        pass
         
 
-    def add_animal(self, rf):
-        self._c.execute("INSERT INTO conversion (rfid) VALUES (?)", (rf, ))
+    def add_animals(self, rfid_list):
+        for rfid in rfid_list:
+            self._c.execute("INSERT INTO conversion (rfid) VALUES (?)", (rfid, ))
+            self._conn.commit()
+
+    def add_animal(self, rfid):
+        self._c.execute("INSERT INTO conversion (rfid) VALUES (?)", (rfid, ))
         self._conn.commit()
 
     def get_all_animals(self):
