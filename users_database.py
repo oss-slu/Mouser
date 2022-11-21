77bb9e65109ed15fbc5cc7fabea87917b7042479
loganwyas <loganw9999@gmail.com>
2022-11-21 15:58:39 -0600

    WIP (Add testing for database functionality)

--- users_database.py
+++ users_database.py
@@ -2,6 +2,11 @@ import sqlite3
 import random
 import string
 
+def get_random_email():
+    letters = string.ascii_lowercase
+    emails = ["gmail.com", "hotmail.com", "aol.com", "yahoo.com", "test.org"]
+    email = (''.join((random.choice(letters)) for i in range(random.randint(10, 20)))) + "@" + random.choice(emails)
+    return email
 class UsersDatabase:
     def __init__(self):
         self.connection = None
@@ -24,20 +29,49 @@ class UsersDatabase:
             
     def add_user(self, email: str, role: str = None):
         if role:
-            self.db.execute('''
-                            INSERT INTO users (email, role) VALUES (?, ?);
-                            ''', (email, role))
+            self.db.execute( "INSERT INTO users (email, role) VALUES (?, ?);", (email, role) )
         else:
-            self.db.execute('''
-                            INSERT INTO users (email) VALUES (?);
-                            ''', (email,))
+            self.db.execute( "INSERT INTO users (email) VALUES (?);", (email,) )
+        self.connection.commit()
+        
+    def get_all_users(self):
+        self.db.execute("SELECT * FROM users")
+        return self.db.fetchall()
+        
+    def get_user_from_id(self, id: int):
+        self.db.execute("SELECT * FROM users WHERE user_id = ?", (id,) )
+        return self.db.fetchall()[0]
+    
+    def get_email_from_id(self, id: int):
+        user = self.get_user_from_id(id)
+        return user[1]
+        
+    def get_role_from_id(self, id: int):
+        user = self.get_user_from_id(id)
+        return user[2]
+    
+    def change_user_email(self, id: int, email: str):
+        self.db.execute("UPDATE users SET email = ? WHERE user_id = ?", (email, id) )
         self.connection.commit()
             
 if __name__ == '__main__':
     database = UsersDatabase()
-    letters = string.ascii_lowercase
-    emails = ["gmail.com", "hotmail.com", "aol.com", "yahoo.com", "test.org"]
-    email = (''.join((random.choice(letters)) for i in range(random.randint(10, 20)))) + "@" + random.choice(emails)
-    database.add_user(email)
+    
+    users = database.get_all_users()
+    print("Users:")
+    for user in users:
+        print(user)
+    
+    print("\nGetting roles from method in class:")
+    for user in users:
+        user_id = user[0]
+        print("User with id", user_id, "has role:", database.get_role_from_id(user_id))
+        
+    print("\nChange random user's email:")
+    user = random.choice(users)
+    user_id = user[0]
+    database.change_user_email(user_id, get_random_email())
+    print("Old user:", user)
+    print("New user:", database.get_user_from_id(user_id))
     
     
