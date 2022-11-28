2b97bc439628cf4e5481e6933a324ced7392cc9e
Cori Diaz <102837772+coridiaz@users.noreply.github.com>
2022-11-28 15:37:02 -0600

    add experiment menu and moved map rfid file
    Signed-off-by: Cori Diaz <102837772+coridiaz@users.noreply.github.com>

--- map_rfid.py
+++ map_rfid.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 from tk_models import *
-from change_rfid import *
+# from experiment_pages.change_rfid import *
 import random
 from playsound import playsound
 import threading
@@ -10,7 +10,7 @@ import threading
 def get_random_rfid():
     return random.randint(1000000, 9999999)
 
-# TODO: Error, doesn't play sound after first attempt
+
 def play_sound_async(filename):
     threading.Thread(target=playsound, args=(filename,), daemon=True).start()
 
@@ -116,15 +116,32 @@ class MapRFIDPage(MouserPage):
         self.changer.open()
 
 
-if __name__ == '__main__':
-    root = Tk()
-    root.title("")
-    root.geometry('600x600')
-    root.resizable(False, False)
 
-    rfid = MapRFIDPage(root)
+class ChangeRFIDDialog():
+    def __init__(self, parent: Tk, map_rfid: MapRFIDPage):
+        self.parent = parent
+        self.map_rfid = map_rfid
 
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_columnconfigure(0, weight=1)
+    def open(self):
+        self.root = root = Toplevel(self.parent)
+        root.title("Change RFID")
+        root.geometry('400x400')
+        root.resizable(False, False)
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
 
-    root.mainloop()
+        simulate_rfid_button = Button(root, text="Simulate RFID", compound=TOP,
+                                      width=15, command=lambda: self.add_random_rfid())
+        simulate_rfid_button.place(relx=0.50, rely=0.20, anchor=CENTER)
+
+        self.root.mainloop()
+
+    def add_random_rfid(self):
+        rfid = get_random_rfid()
+        self.map_rfid.change_selected_value(rfid)
+        self.close()
+
+    def close(self):
+        self.root.destroy()
+
+
