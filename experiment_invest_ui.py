d4a62b0117cf3d3fe16701ac3196b3070d9a65ef
Cori Diaz <102837772+coridiaz@users.noreply.github.com>
2022-11-28 15:51:59 -0600

    moved to correct folder
    Signed-off-by: Cori Diaz <102837772+coridiaz@users.noreply.github.com>

--- experiment_invest_ui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter.ttk import *
-from tk_models import *
-
-
-class InvestigatorsUI(MouserPage):
-    def __init__(self, parent:Tk, prev_page: Frame = None):
-        super().__init__(parent, "Investigators", prev_page)
