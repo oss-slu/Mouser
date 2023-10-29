e36b1466f5039b9ad37f7e09ada34135e62a38ba
stanley yang <stanleyyang2001@gmail.com>
2023-10-29 17:11:34 -0500

    making changes to test.py

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-import serial.tools.list_ports
-import serial
-
+from serial_port_controller import SerialPortController
 
 #this file has nothing to do with main Mouser, just as a test file for different things
-data = []
-ports = list(serial.tools.list_ports.comports())
-print(len(ports))
-for port_ in ports:
-    print("device: ", port_.device, "description: ", port_.description)
+# this file will be used to test the simulator with os walk
+ser = SerialPortController()
+ser.write_info('COM5')
 
-ser = serial.Serial(ports[1].device, 9600, timeout = 1)
-message = "Hello whoever you are\n"
-ser.write(message.encode('ascii'))
-word = ser.readline().decode('ascii')
-print(word)
 
-print('hello')
