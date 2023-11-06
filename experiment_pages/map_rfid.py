51dd6993f6efaa417950f5253f0f2085f752e0c0
rc10283 <99059695+rc10283@users.noreply.github.com>
2023-11-06 13:36:19 -0600

    Autoscroll to Latest Entry
    In Map_RFID, the table automatically scrolls to the newest entry in the table.
    
    Signed-off-by: rc10283 <99059695+rc10283@users.noreply.github.com>

--- experiment_pages/map_rfid.py
+++ experiment_pages/map_rfid.py
@@ -1,11 +1,15 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter.ttk import *
 from tk_models import *
 import tkinter.font as tkfont
 import random
 from playsound import playsound
 import threading
+import webbrowser
 from serial_port_controller import SerialPortController
+from serial import serialutil
+
 
 from database_apis.experiment_database import ExperimentDatabase
 
@@ -17,6 +21,7 @@ def play_sound_async(filename):
     threading.Thread(target=playsound, args=(filename,), daemon=True).start()
 
          
+
 
 class MapRFIDPage(MouserPage):
     def __init__(self, database, parent: Tk, previous_page: Frame = None):
@@ -40,9 +45,11 @@ class MapRFIDPage(MouserPage):
                                       width=15, command=lambda: self.add_random_rfid())
         simulate_rfid_button.place(relx=0.80, rely=0.20, anchor=CENTER)
 
+
         self.table_frame = Frame(self)
-        self.table_frame.place(relx=0.15, rely=0.40)
-        
+        self.table_frame.place(relx=0.15, rely=0.40, relheight= 0.40, relwidth=0.80)
+        self.table_frame.grid_columnconfigure(0, weight= 1)
+        self.table_frame.grid_rowconfigure(0, weight= 1)
         
 
         heading_style = Style()
@@ -56,12 +63,14 @@ class MapRFIDPage(MouserPage):
         self.table.heading('rfid', text='RFID')
 
         self.table.grid(row=0, column=0, sticky='nsew')
+        self.table.grid_columnconfigure(0, weight = 1)
+        self.table.grid_rowconfigure(0, weight = 1)
+
+
         scrollbar = Scrollbar(
             self.table_frame, orient=VERTICAL, command=self.table.yview)
         self.table.configure(yscroll=scrollbar.set)
         scrollbar.grid(row=0, column=1, sticky='ns')
-        self.table.yview_moveto(1)
-        scrollbar.update_idletasks()
 
         self.table.bind('<<TreeviewSelect>>', self.item_selected)
 
@@ -95,8 +104,6 @@ class MapRFIDPage(MouserPage):
             self.table.insert('', END, values=value, tags='text_font')
             self.animals.append(value)
             self.animal_id_entry_text.set(animal)
-            self.table.yview_moveto(1)
-        
 
         ##setting previous button behavior
         self.menu_button = None
@@ -104,8 +111,8 @@ class MapRFIDPage(MouserPage):
         self.menu_page = previous_page
 
         self.menu_button.configure(command = lambda: self.press_back_to_menu_button())
+        self.scroll_to_latest_entry()
     def scroll_to_latest_entry(self):
-        #Scroll to newest Entry
         self.table.yview_moveto(1)
 
     def right_click_menu(self, event):
@@ -124,17 +131,12 @@ class MapRFIDPage(MouserPage):
 
     def add_value(self, rfid):
         self.animal_id = self.db.add_animal(rfid)
-
-
         value = (self.animal_id, rfid)
         self.table.tag_configure('text_font', font=('Arial', 10))
         self.table.insert('', END, values=value, tags='text_font')
         self.animals.append(value)
         self.animal_id_entry_text.set(str(self.animal_id))
         play_sound_async('./sounds/rfid_success.mp3')
-        #When new entry is made, auto scroll to latest entry is called. 
-        #I will rewrite the code in the future to write after 5 entries this should be called. 
-        #As auto scroll is not needed until entry #6. For now this works.
         self.scroll_to_latest_entry()
 
     def change_selected_value(self, rfid):
@@ -193,6 +195,8 @@ class MapRFIDPage(MouserPage):
             self.raise_warning(warning_message= 'Not all animals have been mapped to RFIDs')
         else:
             self.menu_page.tkraise()
+    def close_connection(self):
+        self.db.close()
 
     def close_connection(self):
         self.db.close()
@@ -231,6 +235,7 @@ class SerialPortSelection():
         self.map_rfid = map_rfid
         self.id = None
         self.portController = SerialPortController()
+        self.serial_simulator = SerialSimulator(self.parent)
     
     def open(self):
         root = Toplevel(self.parent)
@@ -254,13 +259,15 @@ class SerialPortSelection():
         #scrollbar
         scrollbar = Scrollbar(root, orient=VERTICAL, command=self.table.yview)
         self.table.configure(yscroll=scrollbar.set)
-        self.table.yview_moveto(1)
         scrollbar.grid(row=0, column=1, sticky='ns')
 
         self.update_ports()
 
         self.select_port = Button(root, text = "Select Port", compound=TOP, width=15, command=self.conform_selection)
         self.select_port.place(relx=0.50, rely=0.85, anchor=CENTER)
+
+        self.run_simulate = Button(root, text = "Run Simulation", compound=TOP, width=15, command=self.open_simulator)
+        self.run_simulate.place(relx=0.75, rely=0.85, anchor=CENTER)
 
 
     def update_ports(self):
@@ -279,5 +286,115 @@ class SerialPortSelection():
         if (self.id != None):
             item_details = self.table.item(self.id)      #port_info = ['port name', 'description']
             port_info = item_details.get("values")
-            #self.portController.read_info(port_info[0])
+            self.portController.set_reader_port(port_info[0])
+            # Todo: complete the implementation of read_info in serial_port_controller
+
+    def open_simulator(self):
+        self.serial_simulator.open()
+
+
+class SerialSimulator():
+    def __init__(self, parent: Tk):
+        self.parent = parent
+        self.serial_controller = SerialPortController()
+        self.written_port = None
+
+    def open(self):
+        if (len(self.serial_controller.get_virtual_port()) == 0):
+            warning = messagebox.askyesno(
+                message=f"Virtual ports missing, would you like to download the virtual ports?",
+                title="Warning"
+                )
+            if (warning):
+                self.download_link()
+              
+        else:
+            self.root = Toplevel(self.parent)
+            self.root.title("Serial Port Selection")
+            self.root.geometry('400x400')
+
+            self.read_message = Text(self.root, height=15, width = 40)
+            self.read_message.place(relx=0.10, rely = 0.00)
+
+            self.drop_down_ports = Combobox(self.root, values=self.serial_controller.get_virtual_port())
+            self.drop_down_ports.place(relx=0.30, rely = 0.88)
+
+            self.comfirm_port = Button(self.root, text="confirm port", width=15,
+                                        command=self.set_written_port)
+            self.comfirm_port.place(relx=0.80, rely=0.900, anchor=CENTER)
+
+            self.input_entry = Entry(self.root, width=40)
+            self.input_entry.place(relx=0.50, rely=0.80, anchor=CENTER)
+
+            self.sent_button = Button(self.root, text = "sent", width = 15, command=self.sent)
+            self.sent_button.place(relx=0.80, rely = 0.80, anchor=CENTER)
+            self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+
+    def sent(self):
+        if (self.written_port != None):
+            print(self.written_port)
+            message = self.input_entry.get()
+            self.serial_controller.write_to(message)
+            self.read_and_display()
+        else:
+            self.raise_warning()
+            """
+            messagebox.showinfo(
+                message=f"Please select a serial port from the drop down list",
+                title="Warning"
+                )"""
+    
+    def setup_ports(self):
+
+        self.serial_controller.set_writer_port(self.written_port)
+
+        available_port = self.serial_controller.get_virtual_port()
+        available_port.remove(self.written_port)
+
+        self.serial_controller.set_reader_port(available_port[0])
+
+
+    def read_and_display(self):
+        available_port = self.serial_controller.get_virtual_port()
+        available_port.remove(self.written_port)
+        if (len(available_port)==0):
+            messagebox.showwarning(
+                message=f"There seems to be problem with the virtual port, please submit bug report.",
+                title="Warning"
+                )
+        else:
+            message = self.serial_controller.read_info()
+            self.read_message.insert(END,message)
+
+
+
+    def check_written_port(self):
+        if (self.written_port == None):
+            return False
+        else:
+            return True
+
+    
+    def set_written_port(self):
+        self.written_port = self.drop_down_ports.get()
+        try:
+            self.setup_ports()
+        except serialutil.SerialException:
+            self.raise_warning()
+
+    def download_link(self):
+        webbrowser.open("https://softradar.com/com0com/")
+
+    def on_closing(self):
+        self.serial_controller.close_all_port()
+        self.written_port = None
+        self.root.destroy()
+
+    def raise_warning(self):
+        message = Toplevel()
+        message.geometry("320x100")
+        message.title('Warning')
+        label = Label(message, text='Please select a serial port from the drop down list')
+        label.grid(row=0, column=0, padx=10, pady=10)
 
