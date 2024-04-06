eca6b88d419085be9a507f033e2a8515677f9d44
stanley yang <stanleyyang2001@gmail.com>
2024-04-06 15:31:58 -0500

    implemented edit/save/import/export functionality of serial port setting

--- shared/serial_port_settings.py
+++ shared/serial_port_settings.py
@@ -1,8 +1,10 @@
 from customtkinter import *
-#from shared.tk_models import SettingPage
-from tk_models import SettingPage
+from shared.tk_models import SettingPage
+#from tk_models import SettingPage
+from csv import *
 from os import listdir, getcwd
 from os.path import isfile, join
+from pathlib import Path
   
 class serialPortSetting(SettingPage):
 
@@ -13,13 +15,11 @@ class serialPortSetting(SettingPage):
         self.title("Serial Port")
 
         self.template_name = StringVar(value="")
-        self.baud_rate = "9600"
+        self.current_template_name = StringVar(value="")
+
         self.baud_rate_var = StringVar(value="9600")
-        self.parity = "None"
         self.parity_var = StringVar(value="None")
-        self.flow_control = "None"
         self.flow_control_var = StringVar(value="None")
-        self.data_bits = "Eight"
         self.data_bits_var = StringVar(value="Eight")
         self.stop_bits_var = StringVar(value="1")
         self.input_bype_var = StringVar(value="Binary")
@@ -28,13 +28,13 @@ class serialPortSetting(SettingPage):
         self.tab_view = CTkTabview(master=self)
         self.tab_view.grid(padx=20, pady=20, sticky = "ew")
 
-        self.tab_view.add("Map RFID")  # add tab at the end
-        self.tab_view.add("Data Collection")  # add tab at the end
+        self.tab_view.add("Map RFID") 
+        self.tab_view.add("Data Collection") 
         self.summary_page("Map RFID")
         self.summary_page("Data Collection")
 
-        #self.port_setting_template_path = os.getcwd() + "\\settings\\serial ports"
-        self.port_setting_template_path = "C:\\Users\\stanl\\capstone1\\mouser\\Mouser\\settings\\serial ports"
+        self.port_setting_template_path = os.getcwd() + "\\settings\\serial ports"
+        #self.port_setting_template_path = "C:\\Users\\stanl\\capstone1\\mouser\\Mouser\\settings\\serial ports"
         self.available_template = [file for file in listdir(self.port_setting_template_path) if isfile(join(self.port_setting_template_path, file))]
 
         for x in self.available_template:
@@ -42,10 +42,11 @@ class serialPortSetting(SettingPage):
 
     
     def edit_page(self, tab: str):
+        # top region of the page
         self.template_region = CTkFrame(master=self.tab_view.tab(tab), corner_radius=10, border_width=2, width=400, height=200)
-        self.region_title_label = CTkLabel(master = self.template_region, text="Existing Template")
-        self.setting_template_label = CTkLabel(self.template_region, text="Current Template", width=8, height=12)
-        self.import_file = CTkOptionMenu(self.template_region, values=self.available_template, height=12, width = 274)
+        self.region_title_label = CTkLabel(master = self.template_region, text="Template")
+        self.setting_template_label = CTkLabel(self.template_region, text="Existing Template", width=8, height=12)
+        self.import_file = CTkOptionMenu(self.template_region, values=self.available_template, variable=self.current_template_name, height=12, width = 274)
         self.edit_template_button = CTkButton(self.template_region, text="Edit", width=2, height=14, command=self.edit_template)
         self.set_preference_button = CTkButton(self.template_region, text="Set Preference", width=2, height=14, command=self.set_preference)
         self.comfirm_button = CTkButton(self.template_region, text="Confirm", width=2, height=14, command=self.confirm_setting)
@@ -58,12 +59,16 @@ class serialPortSetting(SettingPage):
         self.set_preference_button.grid(row=2, column=2, padx=20, pady=15, sticky="ew")
         self.comfirm_button.grid(row=2, column=3, padx=20, pady=15, sticky="ew")
 
+        # bottom region of the page
         self.edit_region = CTkFrame(master=self.tab_view.tab(tab), corner_radius=10, border_width=2, width=400, height=400)
         self.edit_region.grid(row=1, column=0, columnspan=5, padx=20, pady=5, sticky="ew")
 
+        # Naming section
         self.new_template_label = CTkLabel(self.edit_region, text="New Template", width=8, height=12)
         self.new_template_name_label = CTkLabel(self.edit_region, text="Template Name", width=8, height=12)
         self.template_name_entry = CTkEntry(self.edit_region, textvariable=self.template_name)
+
+        # Baud rate section
         self.baud_rate_label = CTkLabel(self.edit_region, text="Baud Rate", width=8, height=12)
         self.baud_rate_menu = CTkOptionMenu(self.edit_region, height=12, 
                                             values=["100", "300", "600", "1200", "2400", "4800", "9600", "19200"], 
@@ -75,6 +80,7 @@ class serialPortSetting(SettingPage):
         self.baud_rate_label.grid(row=2, column=0, padx=20, pady=5, sticky="ew")
         self.baud_rate_menu.grid(row=2, column=1, columnspan=2, padx=20, pady=5, sticky="ew")
 
+        # Parity section
         self.parity_label = CTkLabel(self.edit_region, text="Parity", height=12)
         self.parity_menu = CTkOptionMenu(self.edit_region, height=12, 
                                          values=["None", "Odd", "Even", "Mark", "Space"],
@@ -89,6 +95,7 @@ class serialPortSetting(SettingPage):
         self.flow_control_label.grid(row=4, column=0, padx=20, pady=5, sticky="ew")
         self.flow_control_menu.grid(row=4, column=1, columnspan=2, padx=20, pady=5, sticky="ew")
 
+        # Data bits section
         self.data_bits_label = CTkLabel(self.edit_region, text="Data Bits", height=12)
         self.data_bits_menu = CTkOptionMenu(self.edit_region, height=12, 
                                             values=["Five", "Six", "Seven", "Eight"],
@@ -96,6 +103,7 @@ class serialPortSetting(SettingPage):
         self.data_bits_label.grid(row=5, column=0, padx=20, pady=5, sticky="ew")
         self.data_bits_menu.grid(row=5, column=1, columnspan=2, padx=20, pady=5, sticky="ew") 
 
+        # Stop bit section
         self.stop_bits_label = CTkLabel(self.edit_region, text="Stop Bits", height=12)
         self.one_button = CTkRadioButton(self.edit_region, text="1", variable=self.stop_bits_var, value = "1", height=12)
         self.one_point_five_button = CTkRadioButton(self.edit_region, text="1.5", variable=self.stop_bits_var, value = "1.5", height=12)
@@ -105,6 +113,7 @@ class serialPortSetting(SettingPage):
         self.one_point_five_button.grid(row=6, column=2, padx=20, pady=5, sticky="ew")
         self.two_button.grid(row=7, column=1, padx=20, pady=5, sticky="ew")
 
+        # Input byte section
         self.input_byte_label = CTkLabel(self.edit_region, text="Input Byte", height=12)
         self.binary_button = CTkRadioButton(self.edit_region, text="Binary", variable=self.input_bype_var, value = "Binary", height=12)
         self.text_button = CTkRadioButton(self.edit_region, text="Text", variable=self.input_bype_var, value = "Text", height=12)
@@ -112,31 +121,32 @@ class serialPortSetting(SettingPage):
         self.binary_button.grid(row=8, column=1, padx=20, pady=5, sticky="ew")
         self.text_button.grid(row=8, column=2, padx=20, pady=5, sticky="ew")
 
+        # button
         self.save_button = CTkButton(self.edit_region, text="Save", command=self.save, height=14)
         self.save_button.grid(row=9, column=2, padx=20, pady=40, sticky="ns")
 
     def summary_page(self, tab: str):
 
         self.baud_rate_label = CTkLabel(self.tab_view.tab(tab), text="Baud Rate")
-        self.current_baud_rate = CTkLabel(self.tab_view.tab(tab), text=self.baud_rate)
+        self.current_baud_rate = CTkLabel(self.tab_view.tab(tab), text=self.baud_rate_var.get())
         
         self.baud_rate_label.grid(row=0, column=0, padx=20, pady=5, sticky="ew")
         self.current_baud_rate.grid(row=0, column=2, padx=20, pady=5, sticky="ew")
 
 
         self.parity_label = CTkLabel(self.tab_view.tab(tab), text="Parity")
-        self.current_parity = CTkLabel(self.tab_view.tab(tab), text=self.parity)
+        self.current_parity = CTkLabel(self.tab_view.tab(tab), text=self.parity_var.get())
         self.parity_label.grid(row=1, column=0, padx=20, pady=5, sticky="ew")
         self.current_parity.grid(row=1, column=2, padx=20, pady=5, sticky="ew")
 
         self.flow_control_label = CTkLabel(self.tab_view.tab(tab), text="Flow Control")
-        self.current_flow_control = CTkLabel(self.tab_view.tab(tab), text=self.flow_control)
+        self.current_flow_control = CTkLabel(self.tab_view.tab(tab), text=self.flow_control_var.get())
         self.flow_control_label.grid(row=2, column=0, padx=20, pady=5, sticky="ew")
         self.current_flow_control.grid(row=2, column=2, padx=20, pady=5, sticky="ew")
 
 
         self.data_bits_label = CTkLabel(self.tab_view.tab(tab), text="Data Bits")
-        self.current_data_bits = CTkLabel(self.tab_view.tab(tab), text=self.data_bits)
+        self.current_data_bits = CTkLabel(self.tab_view.tab(tab), text=self.data_bits_var.get())
         self.data_bits_label.grid(row=3, column=0, padx=20, pady=5, sticky="ew")
         self.current_data_bits.grid(row=3, column=2, padx=20, pady=5, sticky="ew")
 
@@ -156,17 +166,10 @@ class serialPortSetting(SettingPage):
 
     def save(self):
 
-        self.baud_rate = self.baud_rate_menu.get()
-        self.parity = self.parity_menu.get()
-        self.flow_control = self.flow_control_menu.get()
-        self.data_bits = self.data_bits_menu.get()
-
-        # TODO: save the setting to serial port directory
-        print("name: ",self.template_name.get())
         file_name = os.path.join(self.port_setting_template_path, self.template_name.get()+".csv")         
 
         file = open(file_name, "w")
-        setting = self.baud_rate+","+self.parity+","+self.flow_control+","+self.data_bits+","+self.stop_bits_var.get()+","+self.input_bype_var.get()
+        setting = self.baud_rate_menu.get()+","+self.parity_menu.get()+","+self.flow_control_menu.get()+","+self.data_bits_menu.get()+","+self.stop_bits_var.get()+","+self.input_bype_var.get()
         file.write(setting)
         file.close() 
 
@@ -191,11 +194,25 @@ class serialPortSetting(SettingPage):
         pass
     
     def confirm_setting(self):
-        # TODO: read from the selected file the associated data used for serial port
-        pass
+        file_name = os.path.join(self.port_setting_template_path, self.current_template_name.get())
+        file = open(file_name)
+        csv_reader = reader(file)
+        for line in csv_reader:
+            self.baud_rate_var.set(line[0])
+            self.parity_var.set(line[1])
+            self.flow_control_var.set(line[2])
+            self.data_bits_var.set(line[3])
+            self.stop_bits_var.set(line[4])
+            self.input_bype_var.set(line[5])
+        file.close()
+
 
     def edit_template(self):
-        #TODO: edit the selected template, possibly bt opening a new window
+        #TODO: edit the selected template, possibly by opening a new window
+        self.template_name_entry.delete(0,END)
+        file_name= Path(self.current_template_name.get())
+        self.template_name_entry.insert(0, file_name.with_suffix(""))
+        self.confirm_setting()
         pass
 
 
