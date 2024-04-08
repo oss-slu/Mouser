d2a4abb3c989ce9c4b5667f22a9c3277e3c7612f
rc10283 <99059695+rc10283@users.noreply.github.com>
2024-04-07 21:08:20 -0500

    Update tk_models.py
    MenuButton Added Back
    
    Signed-off-by: rc10283 <99059695+rc10283@users.noreply.github.com>

--- shared/tk_models.py
+++ shared/tk_models.py
@@ -20,6 +20,14 @@ def create_nav_button(parent: CTkFrame, name: str, button_image: PhotoImage, fra
                     compound=TOP, width=25, command=lambda: raise_frame(frame))
     button.place(relx=relx, rely=rely, anchor=CENTER)
     button.image = button_image
+class MenuButton(CTkButton):
+    '''A standard button that navigates backwards in the program.'''
+    def __init__(self, page: CTkFrame, previous_page: CTkFrame):
+
+        super().__init__(page, text="Back to Menu", compound=TOP,
+                         width=15, command = self.navigate)
+        self.place(relx=0.15, rely=0.15, anchor=CENTER)
+        self.previous_page = previous_page
 
 
 
