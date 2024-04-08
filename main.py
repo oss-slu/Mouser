8a29fe3c11f2b028cde1289120ed6c35f3b58377
Andrew Obermiller <andrew.obermiller@slu.edu>
2024-04-07 22:28:12 -0500

    Changed file opening and saving to work with both protected and unprotected files

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from customtkinter import *
 from CTkMenuBar import *
 from CTkMessagebox import CTkMessagebox
 from shared.tk_models import *
+import shared.file_utils as file_utils
 from experiment_pages.experiment.experiment_menu_ui import ExperimentMenuUI
 from experiment_pages.create_experiment.new_experiment_ui import NewExperimentUI
 from experiment_pages.experiment.select_experiment_ui import ExperimentsUI
 from shared.password_utils import PasswordManager
 
 TEMP_FOLDER_NAME = "Mouser"
+TEMP_FILE_PATH = None
+
+CURRENT_FILE_PATH = None
+PASSWORD = None
 
 def open_file():
     '''Command for 'Open' option in menu bar.
@@ -19,7 +24,11 @@ def open_file():
     Opens a .mouser file
     '''
     file_path = askopenfilename(filetypes=[("Database files",".mouser .pmouser")])
+    print(file_path)
     if file_path:
+        global CURRENT_FILE_PATH 
+        CURRENT_FILE_PATH = file_path
+
         if ".pmouser" in file_path:
             password_prompt = CTkToplevel(root)
             password_prompt.title("Enter Password")
@@ -34,25 +43,18 @@ def open_file():
             def handle_password():
                 password = password_entry.get()
                 try:
-                    manager = PasswordManager(password)
-                    decrypted_data = manager.decrypt_file(file_path)
-                    TEMP_FOLDER_NAME = "Mouser"
-                    temp_folder_path = os.path.join(tempfile.gettempdir(), TEMP_FOLDER_NAME)
-                    os.makedirs(temp_folder_path, exist_ok=True)
-                    temp_file_name =  os.path.basename(file_path)
-                    temp_file_path = os.path.join(temp_folder_path, temp_file_name)
-                    if os.path.exists(temp_file_path):
-                        page = ExperimentMenuUI(root, temp_file_name, experiments_frame)
-                        page.raise_frame()
-                    else:
-                        with open(temp_file_path, "wb") as temp_file:
-                            temp_file.write(decrypted_data)
-                            temp_file.seek(0)
-                            page = ExperimentMenuUI(root, temp_file.name, experiments_frame)
-                            page.raise_frame()
-                        password_prompt.destroy()
 
-                except Exception as _:# pylint: disable= broad-exception-caught
+                    temp_file_path = file_utils.create_temp_from_encrypted(file_path, password)
+                    global PASSWORD
+                    PASSWORD = password
+                    if os.path.exists(temp_file_path):
+                        global TEMP_FILE_PATH
+                        TEMP_FILE_PATH = temp_file_path
+                        page = ExperimentMenuUI(root, temp_file_path, experiments_frame)
+                        page.raise_frame()
+
+                except Exception as e:# pylint: disable= broad-exception-caught
+                    print(e)
                     CTkMessagebox(
                         message="Incorrect password",
                         title="Error",
@@ -62,8 +64,12 @@ def open_file():
             password_button = CTkButton(password_prompt, text="OK", command=handle_password)
             password_button.pack()
         else:
-            page = ExperimentMenuUI(root, file_path, experiments_frame)
+            temp_file = file_utils.create_temp_copy(file_path)
+            global TEMP_FILE_PATH
+            TEMP_FILE_PATH = temp_file
+            page = ExperimentMenuUI(root, temp_file, experiments_frame)
             page.raise_frame()
+        
 
 # Command for 'New' option in menu bar
 def create_file():
@@ -72,6 +78,16 @@ def create_file():
     Navigates to the NewExperimentUI page.'''
     page = NewExperimentUI(root, experiments_frame)
     page.raise_frame()
+
+def save_file():
+    '''Command for the 'save file' option in menu bar.'''
+    print("Current", CURRENT_FILE_PATH)
+    print("Temp", TEMP_FILE_PATH)
+
+    if ".pmouser" in CURRENT_FILE_PATH:
+        file_utils.save_temp_to_encrypted(TEMP_FILE_PATH, CURRENT_FILE_PATH, PASSWORD)
+    else:
+        file_utils.save_temp_to_file(TEMP_FILE_PATH, CURRENT_FILE_PATH)
 
 TEMP_FOLDER_NAME = "Mouser"
 temp_folder_path = os.path.join(tempfile.gettempdir(), TEMP_FOLDER_NAME)
@@ -89,6 +105,7 @@ file_menu = menu_bar.add_cascade("File")
 file_dropdown = CustomDropdownMenu(widget=file_menu)
 file_dropdown.add_option(option="New", command = create_file)
 file_dropdown.add_option(option="Open", command = open_file)
+file_dropdown.add_option(option="Save", command = save_file)
 
 root.config(menu=menu_bar)
 
---
+++ shared/file_utils.py
@@ -0,0 +1,63 @@
+import tempfile, os
+from shared.password_utils import PasswordManager
+
+
+TEMP_FOLDER_NAME = "Mouser"
+
+def create_temp_copy(filepath:str):
+    '''Creates a new temporary file and returns the file path of the temporary file.'''
+
+    temp_folder_path = os.path.join(tempfile.gettempdir(), TEMP_FOLDER_NAME)
+    os.makedirs(temp_folder_path, exist_ok=True)
+    temp_file_name =  os.path.basename(filepath)
+    temp_file_path = os.path.join(temp_folder_path, temp_file_name)
+
+    with open(filepath, 'rb') as file:
+        data = file.read()
+
+    with open(temp_file_path, 'wb') as file:
+        file.write(data)
+        file.seek(0)
+    
+    return temp_file_path
+
+def create_temp_from_encrypted(filepath:str, password:str):
+    '''Creates a new decrypted copy of a file.'''
+
+    print(filepath)
+    temp_folder_path = os.path.join(tempfile.gettempdir(), TEMP_FOLDER_NAME)
+    os.makedirs(temp_folder_path, exist_ok=True)
+    temp_file_name =  os.path.basename(filepath)
+    temp_file_path = os.path.join(temp_folder_path, temp_file_name)
+
+    manager = PasswordManager(password)
+
+    data = manager.decrypt_file(filepath)
+
+    with open(temp_file_path, 'wb') as file:
+        file.write(data)
+        file.seek(0)
+    
+    return temp_file_path
+
+
+def save_temp_to_file(temp_file_path: str, permanent_file_path: str):
+    '''Save data from temporary file to a permanent file'''
+    
+    with open(temp_file_path, 'rb') as temp_file:
+        data = temp_file.read()
+    
+    with open(permanent_file_path, 'wb') as file:
+        file.write(data)
+    
+def save_temp_to_encrypted(temp_file_path: str, permanent_file_path: str, password:str):
+    '''Save data from temporary file to an encrypted file.'''
+    manager = PasswordManager(password)
+    
+    manager.encrypt_file(temp_file_path)
+    with open(temp_file_path, 'rb') as encrypted:
+        data = encrypted.read()
+    
+    with open(permanent_file_path, 'wb') as file:
+        file.write(data)
+    
