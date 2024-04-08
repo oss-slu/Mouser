a7c4a6c74e4227ce317a53720e845974104df521
rc10283 <99059695+rc10283@users.noreply.github.com>
2024-04-07 20:42:11 -0500

    Update main.py
    Updates Main for Mouser Logo and direction of the button clicked to the new experiments page.
    
    Signed-off-by: rc10283 <99059695+rc10283@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-'''Main functionality of Program.'''
 import shutil
 import tempfile
+import os
 from tkinter.filedialog import *
 from customtkinter import *
 from CTkMenuBar import *
@@ -10,7 +10,8 @@ from experiment_pages.experiment.experiment_menu_ui import ExperimentMenuUI
 from experiment_pages.create_experiment.new_experiment_ui import NewExperimentUI
 from experiment_pages.experiment.select_experiment_ui import ExperimentsUI
 from shared.password_utils import PasswordManager
-
+from experiment_pages.experiment.select_experiment_ui import ExperimentsUI
+from PIL import Image
 
 def open_file():
     '''Command for 'Open' option in menu bar.
@@ -80,31 +81,37 @@ if os.path.exists(temp_folder_path):
 root = CTk()
 root.title("Mouser")
 root.geometry('900x600')
-root.minsize(900,600)
+root.minsize(900, 600)
+
 
 # Adds menu bar to root and binds the function to file_menu
 menu_bar = CTkMenuBar(root)
 file_menu = menu_bar.add_cascade("File")
 file_dropdown = CustomDropdownMenu(widget=file_menu)
-file_dropdown.add_option(option="New", command = create_file)
-file_dropdown.add_option(option="Open", command = open_file)
+file_dropdown.add_option(option="New", command=create_file)
+file_dropdown.add_option(option="Open", command=open_file)
 
 root.config(menu=menu_bar)
-
-
 main_frame = MouserPage(root, "Mouser")
-
-
 experiments_frame = ExperimentsUI(root, main_frame)
+def on_mouse_button_click():
+    raise_frame(experiments_frame)
 
-raise_frame(experiments_frame)
-mouse_image = CTk.CTkImage(file="./shared/images/MouseLogo.png")
-create_nav_button(main_frame, "Mouser", mouse_image,
-                  experiments_frame, size=(30,30))
+# Display mouse image button on main frame
+mouse_image = CTkImage(light_image=Image.open("./shared/images/MouseLogo.png"), size=(550, 200))
+mouse_button = CTkButton(main_frame, image=mouse_image, command=on_mouse_button_click)
+mouse_button.grid(row=1, column=0, pady=(20, 10)) 
+create_nav_button(main_frame, "Welcome to Mouser! Please click the Logo to open a new Experiment.\nSelect the File Button at the top left of the screen to open an existing experiment.", mouse_image,experiments_frame, 0.5, 0.33)
 
+
+raise_frame(main_frame)
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
+
+# Ensure the main_frame widget expands to fill the grid cell it's in, which helps with alignment.
+main_frame.grid_rowconfigure(1, weight=1)  # Adjust '1' if other rows are used; make it expandable
+main_frame.grid_columnconfigure(0, weight=1)  # Make the column containing the label expandable
 
 root.mainloop()
 
