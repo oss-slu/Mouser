df77f6838ecc308b6fb6ae453232ec7b3ab69ae0
Jack Belyeu <jackbelyeu@gmail.com>
2024-09-23 19:42:53 -0500

    logic inverted for proper functionality

--- experiment_pages/experiment/experiment_menu_ui.py
+++ experiment_pages/experiment/experiment_menu_ui.py
@@ -133,7 +133,8 @@ class ExperimentMenuUI(MouserPage): #pylint: disable= undefined-variable
     
     def disable_buttons_if_needed(self):
     # This method disables all buttons except for the Map RFID button until all specimens have an associated RFID
-        if self.all_rfid_mapped():
+        self.group_button.configure(state="normal")
+        if not self.all_rfid_mapped():
             self.collection_button.configure(state="disabled")
             self.analysis_button.configure(state="disabled")
             self.group_button.configure(state="disabled")
@@ -141,6 +142,7 @@ class ExperimentMenuUI(MouserPage): #pylint: disable= undefined-variable
             self.collection_button.configure(state="normal")
             self.analysis_button.configure(state="normal")
             self.group_button.configure(state="normal")
+
 
     def on_show_frame(self):
         self.disable_buttons_if_needed()
