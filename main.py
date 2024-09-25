8727db44f358160f47aedab10f47638bc2dc0d4d
Jack Belyeu <jackbelyeu@gmail.com>
2024-09-25 12:09:32 -0500

    r

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_resource_path(relative_path):
 
 
 # Ensure we are passing only the correct path
-csv_file_path = get_resource_path("settings/serial ports/preference/serial_port_preference.csv")
+csv_file_path = get_resource_path(r"settings/serial ports/preference/serial_port_preference.csv")
 csv_file_path = os.path.normpath(csv_file_path)
 print(f"CSV file path: {csv_file_path}")  # Debugging: print the path to verify it's correct
 
