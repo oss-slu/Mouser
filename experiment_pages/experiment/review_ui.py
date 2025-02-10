07cb29739f828a18a0bfc0bbd16a62e7afe3fe3f
--unset <--unset>
2025-02-10 15:42:03 -0600

    gg linter

--- experiment_pages/experiment/review_ui.py
+++ experiment_pages/experiment/review_ui.py
@@ -1,6 +1,5 @@
 from databases.experiment_database import ExperimentDatabase
 from shared.tk_models import *
-from shared.scrollable_frame import ScrolledFrame
 
 class ReviewUI(MouserPage):
     '''Review User Interface for experiment summary.'''
@@ -11,18 +10,18 @@ class ReviewUI(MouserPage):
 
         self.main_frame = CTkFrame(self, corner_radius=15)
         self.main_frame.grid(row=0, column=0, sticky='NESW')
-        self.main_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.main_frame.place(relx=0.5, rely=0.5, anchor=CENTER) #pylint: disable = undefined-variable
 
         # Example usage with getter
         name_label = CTkLabel(
             self.main_frame,
-            text=f"Experiment name: {self.database.get_name()} /n Animals: {self.database.get_measurement_items()}",
+            text=f"Experiment name: {self.database.get_name()}",
         )
-        name_label.place(relx=0.5, rely=0.1, anchor=CENTER)
+        name_label.place(relx=0.5, rely=0.1, anchor=CENTER) #pylint: disable = undefined-variable
 
         # Adding additional text
         additional_text_label = CTkLabel(
             self.main_frame,
             text="Additional information goes here."
         )
-        additional_text_label.place(relx=0.5, rely=0.2, anchor=CENTER)
+        additional_text_label.place(relx=0.5, rely=0.2, anchor=CENTER) #pylint: disable = undefined-variable
