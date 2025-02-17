b22bb40b726d5d3e2615ce43ea9bdede36f3fc21
NNewbauer <nnewbauer@slu.edu>
2025-02-17 14:52:13 -0600

    Additional changes to conditional statement to ensure second opening of file works

--- experiment_pages/experiment/data_collection_ui.py
+++ experiment_pages/experiment/data_collection_ui.py
@@ -27,12 +27,15 @@ class DataCollectionUI(MouserPage):
         self.measurement_items = self.database.get_measurement_items()
         
         ## ENSURE ANIMALS ARE IN DATABASE BEFORE EXPERIMENT FOR EXPERIMENTS W/O RFID ##
-        if self.database.experiment_uses_rfid() != 1:
+        if self.database.experiment_uses_rfid() != 1 and self.database.get_animals() == []:
+            print(self.database.get_animals())
             i = 1
             max_num_animals = self.database.get_number_animals()
             while i <= max_num_animals:
                 self.database.add_animal(i, i)
                 i = i + 1
+        print(self.database.get_animals())
+
 
 
         self.measurement_strings = []
